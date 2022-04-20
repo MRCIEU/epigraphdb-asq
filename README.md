d11bef17ce806374f5fa40cd56befc1a087d7700
YiLiu6240 <yi.liu6240@gmail.com>
2022-04-20 10:30:06 +0100

    Update citation

--- README.md
+++ README.md
@@ -14,4 +14,20 @@ For feedback and questions please use [Github issues](https://github.com/mrcieu/
 
 ## Citation
 
-Forthcoming.
+**Triangulating evidence in health sciences with Annotated Semantic Queries**,
+Yi Liu, Tom R Gaunt,
+medRxiv 2022.04.12.22273803; doi: https://doi.org/10.1101/2022.04.12.22273803
+
+```
+@article{Liu-Gaunt-2022-ASQ,
+  author = {Liu, Yi and Gaunt, Tom R},
+  title = {Triangulating evidence in health sciences with Annotated Semantic Queries},
+  elocation-id = {2022.04.12.22273803},
+  year = {2022},
+  doi = {10.1101/2022.04.12.22273803},
+  publisher = {Cold Spring Harbor Laboratory Press},
+  URL = {https://www.medrxiv.org/content/early/2022/04/16/2022.04.12.22273803},
+  eprint = {https://www.medrxiv.org/content/early/2022/04/16/2022.04.12.22273803.full.pdf},
+  journal = {medRxiv}
+}
+```
--- frontend/src/resources/docs/docs-view.ts
+++ frontend/src/resources/docs/docs-view.ts
@@ -57,7 +57,21 @@ Further details can be found at the respective documentation of these concepts.
 export const aboutCitation: string = `
 ## Citation
 
-Propoer citation details will be available shortly.
-Until then please cite this website https://asq.epigraphdb.org
-if you use ASQ.
+**Triangulating evidence in health sciences with Annotated Semantic Queries**,
+Yi Liu, Tom R Gaunt,
+medRxiv 2022.04.12.22273803; doi: https://doi.org/10.1101/2022.04.12.22273803
+
+\`\`\`
+@article{Liu-Gaunt-2022-ASQ,
+  author = {Liu, Yi and Gaunt, Tom R},
+  title = {Triangulating evidence in health sciences with Annotated Semantic Queries},
+  elocation-id = {2022.04.12.22273803},
+  year = {2022},
+  doi = {10.1101/2022.04.12.22273803},
+  publisher = {Cold Spring Harbor Laboratory Press},
+  URL = {https://www.medrxiv.org/content/early/2022/04/16/2022.04.12.22273803},
+  eprint = {https://www.medrxiv.org/content/early/2022/04/16/2022.04.12.22273803.full.pdf},
+  journal = {medRxiv}
+}
+\`\`\`
 `;
